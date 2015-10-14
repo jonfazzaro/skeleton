fb4dd8131aa271296a2122ff74220c51a85265a9
jonfazzaro <jon@fazzaro.com>
2015-10-14 08:07:53 -0400

    Adding field constants.

--- web/TeamFoundation/FieldNames.cs
+++ web/TeamFoundation/FieldNames.cs
@@ -1,5 +1,8 @@
 ﻿namespace Skeleton.TeamFoundation {
     public class FieldNames {
-        public const string Priority = "Microsoft.VSTS.Common.BacklogPriority";
+        public const string BacklogPriority = "Microsoft.VSTS.Common.BacklogPriority";
+        public const string StackRank = "Microsoft.VSTS.Common.StackRank";
+        public const string WorkItemType = "System.WorkItemType";
+        public const string Title = "System.Title";
     }
 }
