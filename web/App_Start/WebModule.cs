e277e3f3e374e6cdb544a8e0bf4a62f5f24f4ba8
Jon Fazzaro <jfazzaro@apterainc.com>
2016-11-03 13:05:11 -0400

    Repair Session scope issue

--- web/App_Start/WebModule.cs
+++ web/App_Start/WebModule.cs
@@ -1,15 +1,16 @@
 ﻿namespace Skeleton.Web.App_Start {
     using Cards;
     using Ninject.Modules;
+    using Ninject.Web.Common;
     using State;
     using State.Web;
     using TeamFoundation;
 
     public class WebModule : NinjectModule {
         public override void Load() {
-            Bind<ISessionProvider>().To<HttpSessionProvider>();
-            Bind<ICardsClient>().To<TeamFoundationClient>();
-            Bind<IProjectsClient>().To<TeamFoundationClient>();
+            Bind<ISessionProvider>().To<HttpSessionProvider>().InRequestScope();
+            Bind<ICardsClient>().To<TeamFoundationClient>().InRequestScope();
+            Bind<IProjectsClient>().To<TeamFoundationClient>().InRequestScope();
         }
     }
 }
