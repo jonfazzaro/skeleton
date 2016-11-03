0566251f1ae0abece71b7009fa959102e7e1e36e
Jon Fazzaro <jfazzaro@apterainc.com>
2016-11-03 11:48:24 -0400

    Add AI logging for API.

--- web/App_Start/WebApiConfig.cs
+++ web/App_Start/WebApiConfig.cs
@@ -1,10 +1,12 @@
 ﻿namespace Skeleton.Web {
     using System.Web.Http;
+    using System.Web.Http.ExceptionHandling;
+    using Telemetry;
 
     public static class WebApiConfig {
         public static void Register(HttpConfiguration config) {
             config.MapHttpAttributeRoutes();
-
+            config.Services.Add(typeof(IExceptionLogger), new AiExceptionLogger());
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ web/Telemetry/AiExceptionLogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Skeleton.Web.Telemetry {
+    using System.Web.Http.ExceptionHandling;
+
+    public class AiExceptionLogger : ExceptionLogger {
+
+        public override void Log(ExceptionLoggerContext context) {
+            var ai = new Microsoft.ApplicationInsights.TelemetryClient();
+            ai.TrackException(context.Exception);
+        }
+    }
+}
