1be259afb9e4cb4f772be6c5ca996017bb6d9d8d
Jon Fazzaro <jfazzaro@apterainc.com>
2016-11-03 11:32:10 -0400

    Embed AI telemetry.

---
+++ web/FilterConfig.cs
@@ -0,0 +1,11 @@
+using System.Web.Mvc;
+
+namespace Skeleton.Web{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+        }
+    }
+}
